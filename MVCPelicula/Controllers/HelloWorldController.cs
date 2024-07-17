bb470d5bf9ce9627840a81ed70720e43f8875c15
karivas-dev <72422927+karivas-dev@users.noreply.github.com>
2024-07-17 15:54:41 -0600

    Initial commit

---
+++ MVCPelicula/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCPelicula.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string nombre, string apellido, int numVeces)
+        {
+            ViewData["numVeces"] = numVeces;
+            ViewData["nombre"] = nombre;
+            ViewData["apellido"] = apellido;
+
+            return View();
+        }
+    }
+}
